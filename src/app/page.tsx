10e9869f23fb7623f93995ed59544a1f1d6520b1
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-08-03 19:33:15 +0200

    feat/fix: added loader and fixed some issues regarding baseurl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <main>
+        <h1 className='text-6xl text-center m-5 font-bold'> sURLs </h1>
         <Upload />
       </main>
     </div>
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,6 +1,8 @@
 'use client';
 import axios from 'axios';
 import { useState } from 'react';
+import loader from '../assets/loader.svg';
+import Image from 'next/image';
 
 type ShortenedURLObject = {
     success: boolean, alreadyExists: boolean,
@@ -14,18 +16,24 @@ function Upload() {
     const [shortenedURLObject, setShortenedURLObject] = useState<ShortenedURLObject | null>(null);
     const [url, setUrl] = useState('');
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = (event: any) => {
         event.preventDefault();
+
+        // Set loading to true
+        setLoading(true);
 
         axios.post('/api/upload', {
             url: event.target[0].value
         }).then((response) => {
             setShortenedURLObject({ ...response.data });
             setError('');
+            setLoading(false);
         }).catch((error) => {
             setError(error.response.data.message);
             setShortenedURLObject(null);
+            setLoading(false);
         });
     };
 
@@ -33,26 +41,32 @@ function Upload() {
         if (shortenedURLObject?.response) {
             return (
                 <div className="mt-4 p-2 bg-white shadow-md rounded-md">
-                    <p className="text-center text-gray-700 font-bold">Original URL:</p>
-                    <a href={shortenedURLObject.response.url} target="_blank" rel="noopener noreferrer" className="block text-center text-blue-500 font-bold">{shortenedURLObject.response.url}</a>
-                    <hr className="my-2" />
-                    <p className="text-center text-gray-700 font-bold">Shortened URL:</p>
-                    <a href={`${process.env.BASE_URL}/r/${shortenedURLObject.response.shortenedURL}`} target="_blank" rel="noopener noreferrer" className="block text-center text-blue-500 font-bold">{`${process.env.BASE_URL}/r/${shortenedURLObject.response.shortenedURL}`}</a>
+                    <div className='w-full'>
+                        <p className="text-center text-gray-700 font-bold">Original URL:</p>
+                        <div className="block text-center text-blue-500 font-bold break-all p-2"><a href={shortenedURLObject.response.url} target="_blank" rel="noopener noreferrer"> {shortenedURLObject.response.url} </a></div>
+                    </div>
+                    <div>
+                        <hr className="my-2" />
+                        <p className="text-center text-gray-700 font-bold">Shortened URL:</p>
+                        <div className="block text-center text-blue-500 font-bold break-all p-2"><a href={`${process.env.BASE_URL}/r/${shortenedURLObject.response.shortenedURL}`} target="_blank" rel="noopener noreferrer"> {`${process.env.BASE_URL}/r/${shortenedURLObject.response.shortenedURL}`}</a></div>
+                    </div>
                 </div>
             );
         }
     };
 
     return (
-        <div>
-            <div className=''>
+        <div className='max-w-md'>
+            <div>
                 <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-md p-6 flex w-full justify-center">
                     <div className='flex space-x-2'>
                         <div>
                             <input value={url} onChange={(e) => setUrl(e.target.value)} type="text" id="url" name="url" placeholder="Enter a URL" className="border border-gray-400 p-2 w-full rounded-md" required />
                         </div>
                         <div className='text-center'>
-                            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">Shorten</button>
+                            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+                                {loading ? <Image src={loader} width={25} height={25} alt='Loader' className='w-full h-full' /> : 'Shorten'}
+                            </button>
                         </div>
                     </div>
                 </form>
