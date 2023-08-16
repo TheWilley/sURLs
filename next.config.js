e4f60735166d440fe9572650a198e5a3f58c7da4
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-08-16 09:45:03 +0200

    dev: removed uneeded .env variable in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        BASE_URL: process.env.BASE_URL
-    },
     output: process.env.USE_DOCKER === true ? 'standalone' : undefined
 }
 
